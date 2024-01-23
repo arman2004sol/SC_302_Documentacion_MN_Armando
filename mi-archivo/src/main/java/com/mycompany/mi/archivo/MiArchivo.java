9f327c23ca95074016ba04e19811602529701c7e
arman2004sol <138852305+arman2004sol@users.noreply.github.com>
2024-01-23 15:17:54 -0600

    Add files via upload

---
+++ mi-archivo/src/main/java/com/mycompany/mi/archivo/MiArchivo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mi.archivo;
+
+/**
+ *
+ * @author armi8
+ */
+public class MiArchivo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
